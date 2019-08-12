22edd36f7844d540e2c3a0e1b87077172dbfe301
TS.SHE.gongjianan <gongjianan@travelsky.com>
2019-08-12 09:50:47 +0800

    Add a new file

---
+++ Demo1.java
@@ -0,0 +1,13 @@
+package com.gong.test;
+
+public class Demo1 {
+	
+	System.out.print("aaa");
+	System.out.print("bbb");
+	System.out.print("ccc");
+
+
+	System.out.print("ddd");
+	System.out.print("eee");
+	System.out.print("fff");
+}
