fbe8a8cfb6825e873adf88ba6390bbcf654744c8
TS.SHE.gongjianan <gongjianan@travelsky.com>
2019-08-20 10:27:50 +0800

    modyfy by test1 2019-8-20 10:27:47

--- Demo1.java
+++ Demo1.java
@@ -7,7 +7,7 @@ public class Demo1 {
 	System.out.print("ccc");
 
 	System.out.print("add by:test_dev");
-	System.out.print("add by:test_dev. modify by test_dev:0820-1018");
+	System.out.print("add by:test_dev. modify by test_dev:0820-1026");
 	System.out.print("ddd");
 	System.out.print("eee");
 	System.out.print("fff");
